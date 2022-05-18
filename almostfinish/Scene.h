ad1758cee2d9906e3219bf37b28a5ca53d6ea764
Русаков Андрей <70331127+ochenudivlen@users.noreply.github.com>
2022-05-18 18:18:53 +0300

    Add files via upload

--- almostfinish/Scene.h
+++ almostfinish/Scene.h
@@ -1,17 +1,19 @@
-#pragma once
+﻿#pragma once
 #include <SFML/Graphics.hpp>
+#include <stdlib.h>
 #include "Pistol.h"
 #include "Shotgun.h"
 #include "Hero.h"
 #include <vector>
 #include "Enemy.h"
 #include "Map.h"
+
 const float renderTimeShoot = 50;
 
 using namespace std;
-struct Scene: public sf::RenderWindow{
+struct Scene : public sf::RenderWindow {
     int width, height;
-    Scene(int width, int height): sf::RenderWindow(sf::VideoMode(width, height), "Star Fighter"){
+    Scene(int width, int height) : sf::RenderWindow(sf::VideoMode(width, height), "Star Fighter") {
         this->width = width;
         this->height = height;
     }
@@ -27,76 +29,62 @@ struct Scene: public sf::RenderWindow{
     vector<Bullet> Bullets;
     int itBullets = 0;
 
-//    sf::Sprite background;
-//    void initializeTexture(){
-//        sf::Texture mapTexture;
-//        if(!mapTexture.loadFromFile("Space.jpg")){
-//            cout << "Cant load map";
-//        }
-//        mapTexture.setSmooth(true);
-//        background.setTexture(mapTexture);
-//
-//    }
-    //  bool IsForceApplied(size_t birdIndex, const World&) const
-    //  {
-    //  }
-
-   
     int handleBarier(Hero& hero, Map& objects)
+    {
+        for (size_t i = 0; i < objects.obj.size(); ++i)
         {
-            for (size_t i = 0; i < objects.obj.size(); ++i)
+            if ((abs(hero.getPosition().y - objects.obj[i].getPosition().y) <= objects.sizeObj / sqrt(2) + hero.radius) &&
+                (abs(hero.getPosition().x - objects.obj[i].getPosition().x) <= objects.sizeObj / sqrt(2) + hero.radius))
             {
-                if ((abs(hero.getPosition().y - objects.obj[i].getPosition().y) <= objects.sizeObj / sqrt(2) + hero.radius) &&
-                    (abs(hero.getPosition().x - objects.obj[i].getPosition().x) <= objects.sizeObj / sqrt(2) + hero.radius))
-                {
-                    return i;
-                }
+                return i;
             }
-            return -1;
+        }
+        return -1;
+    }
+
+    void handleEvents(Hero& hero, sf::Vector2i mousePosition, Map& objects, vector<Enemy>& enemies)
+    {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        {
+            if ((hero.getPosition().x > hero.radius) && (handleBarier(hero, objects) == -1))
+                hero.move(-hero.velocity, 0);
+            else
+                if ((hero.getPosition().x > hero.radius) && (objects.obj[handleBarier(hero, objects)].getPosition().x + objects.sizeObj / sqrt(2) > hero.getPosition().x))
+                    hero.move(-hero.velocity, 0);
         }
 
-        void handleEvents(Hero& hero, sf::Vector2i mousePosition, Map& objects, vector<Enemy> &enemies)
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
         {
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-            {
-                if ((hero.getPosition().x > hero.radius) && (handleBarier(hero, objects) == -1))
-                    hero.move(-hero.velocity, 0);
-                else
-                    if ((hero.getPosition().x > hero.radius) && (objects.obj[handleBarier(hero, objects)].getPosition().x + objects.sizeObj / sqrt(2) > hero.getPosition().x))
-                        hero.move(-hero.velocity, 0);
-            }
-
-            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-            {
-                if ((hero.getPosition().x < width - hero.radius) && (handleBarier(hero, objects) == -1))
+            if ((hero.getPosition().x < width - hero.radius) && (handleBarier(hero, objects) == -1))
+                hero.move(hero.velocity, 0);
+            else
+                if ((hero.getPosition().x < width - hero.radius) && (objects.obj[handleBarier(hero, objects)].getPosition().x - objects.sizeObj / sqrt(2) < hero.getPosition().x))
                     hero.move(hero.velocity, 0);
-                else
-                    if((hero.getPosition().x < width - hero.radius) && (objects.obj[handleBarier(hero, objects)].getPosition().x - objects.sizeObj / sqrt(2) < hero.getPosition().x))
-                        hero.move(hero.velocity, 0);
-            }
+        }
 
-            else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-            {
-                if ((hero.getPosition().y > hero.radius) && (handleBarier(hero, objects) == -1))
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+        {
+            if ((hero.getPosition().y > hero.radius) && (handleBarier(hero, objects) == -1))
+                hero.move(0, -hero.velocity);
+            else
+                if ((hero.getPosition().y > hero.radius) && (objects.obj[handleBarier(hero, objects)].getPosition().y + objects.sizeObj / sqrt(2) > hero.getPosition().y))
                     hero.move(0, -hero.velocity);
-                else
-                    if ((hero.getPosition().y > hero.radius) && (objects.obj[handleBarier(hero, objects)].getPosition().y + objects.sizeObj / sqrt(2) > hero.getPosition().y))
-                        hero.move(0, -hero.velocity);
         }
 
         else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
         {
-                if ((hero.getPosition().y < height - hero.radius) && (handleBarier(hero, objects) == -1))
-                    hero.move(0, hero.velocity);
+            if ((hero.getPosition().y < height - hero.radius) && (handleBarier(hero, objects) == -1))
+                hero.move(0, hero.velocity);
             else
-                    if ((hero.getPosition().y < height - hero.radius) && (objects.obj[handleBarier(hero, objects)].getPosition().y - objects.sizeObj / sqrt(2) < hero.getPosition().y))
-          
+                if ((hero.getPosition().y < height - hero.radius) && (objects.obj[handleBarier(hero, objects)].getPosition().y - objects.sizeObj / sqrt(2) < hero.getPosition().y))
+
                     hero.move(0, hero.velocity);
         }
-        
+
         time = clock.getElapsedTime();
         curTime = time.asMilliseconds();
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::Q) && curTime - pistol.timeLastShoot > pistol.delayShoot){
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q) && curTime - pistol.timeLastShoot > pistol.delayShoot) {
             pistol.timeLastShoot = curTime;
             itBullets++;
             vector<Bullet> temp = pistol.shoot(hero.getPosition(), mousePosition);
@@ -105,69 +93,115 @@ struct Scene: public sf::RenderWindow{
 
         time = clock.getElapsedTime();
         curTime = time.asMilliseconds();
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::E) && curTime - shotgun.timeLastShoot > shotgun.delayShoot){
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::E) && curTime - shotgun.timeLastShoot > shotgun.delayShoot) {
             shotgun.timeLastShoot = curTime;
             itBullets += 3;
             vector<Bullet> temp = shotgun.shoot(hero.getPosition(), mousePosition);
             Bullets.insert(Bullets.end(), temp.begin(), temp.end());
         }
-        if(curTime - prev > pistol.delayShoot) {
+
+        if (curTime - prev > pistol.delayShoot) {
             prev = curTime;
-        for(int l = 0; l < Enemy::count; ++l){
-            enemies[l].shoot(hero.getPosition(), curTime);
-        }
+            for (int l = 0; l < Enemy::count; ++l) {
+                enemies[l].shoot(hero.getPosition(), curTime);
+            }
         }
     }
 
     float prevTimeShoot1 = 0;
-    void Draw(vector<Enemy> &enemies, Hero hero, Map structers, sf::Sprite background)
+    void Draw(vector<Enemy>& enemies, Hero& hero, Map structers, sf::Sprite background)
     {
         //        draw(background);
-                draw(structers.obj[0]);
-                draw(hero);
+        draw(structers.obj[0]);
+        draw(hero);
 
-
-        for(int l = 0; l < Enemy::count; ++l) {
-            if(enemies[l].it > 0) {
-                handleBullets(enemies[l].Bullets, enemies, structers, enemies[l].prewTimeShoot, enemies[l].it);
-                for(int k = 0; k < enemies[l].it; ++k) {
+        for (int l = 0; l < Enemy::count; ++l) {
+            if (enemies[l].it > 0) {
+                handleBullets(enemies[l].Bullets, enemies, structers, hero, enemies[l].prewTimeShoot, enemies[l].it);
+                for (int k = 0; k < enemies[l].it; ++k) {
                     draw(counterWayBullet(enemies[l].Bullets[k]));
                 }
             }
-            draw(enemies[l]);
+                draw(enemies[l]);
         }
 
-
         // drawing bullets
-        if(itBullets > 0) {
-            handleBullets(Bullets, enemies, structers, prevTimeShoot, itBullets);
-            for(int k = 0; k < itBullets; ++k)
+        if (itBullets > 0) {
+            handleBullets(Bullets, enemies, structers, hero, prevTimeShoot, itBullets);
+            for (int k = 0; k < itBullets; ++k)
                 draw(counterWayBullet(Bullets[k]));
         }
     }
 
+    void handleBullets(vector<Bullet>& Bullets, vector<Enemy>& enemies, Map& objects, Hero& hero, float& prevTimeShoot, int& itBullets)
+    {
+        time = clock.getElapsedTime();
+        curTime1 = time.asMilliseconds();
 
-
-    
-    
-    
-    
-    
-    
-    void handleBullets(vector<Bullet>& Bullets, vector<Enemy>& enemies, Map& objects, float& prevTimeShoot, int& itBullets)
+        if (curTime1 - prevTimeShoot > renderTimeShoot)
         {
-            time = clock.getElapsedTime();
-            curTime1 = time.asMilliseconds();
+            prevTimeShoot = curTime1;
 
-            if (curTime1 - prevTimeShoot > renderTimeShoot)
+            for (int k = 0; k < itBullets; ++k) //Ïðîâåðêà íà âûõîä ïóëè çà ïðåäåë ýêðàíà
             {
-                prevTimeShoot = curTime1;
+                Bullets[k].it += 5;
 
-                for (int k = 0; k < itBullets; ++k) //Ïðîâåðêà íà âûõîä ïóëè çà ïðåäåë ýêðàíà
+                if (Bullets[k].it * Bullets[k].velocity > sqrt(width * width + height * height))
                 {
-                    Bullets[k].it += 5;
+                    Bullet temp;
+                    temp = Bullets[k];
+                    Bullets[k] = Bullets[itBullets - 1];
+                    Bullets[itBullets - 1] = temp;
+                    itBullets--;
+                    Bullets.resize(itBullets);
+                }
+            }
 
-                    if (Bullets[k].it * Bullets[k].velocity > sqrt(width * width + height * height))
+            for (int k = 0; k < itBullets; ++k) //Проверка на попадение пули во врага
+            {
+                for (int l = 0; l < Enemy::count; ++l)
+                {
+                    float t_x = abs((Bullets[k].r0 + Bullets[k].size * Bullets[k].direction + Bullets[k].it * Bullets[k].velocity * Bullets[k].direction).x - enemies[l].getPosition().x);
+                    float t_y = abs((Bullets[k].r0 + Bullets[k].size * Bullets[k].direction + Bullets[k].it * Bullets[k].velocity * Bullets[k].direction).y - enemies[l].getPosition().y);
+
+                    if ((t_x < enemies[l].radius) && (t_y < enemies[l].radius))
+                    {
+                        Bullet temp;
+                        temp = Bullets[k];
+                        Bullets[k] = Bullets[itBullets - 1];
+                        Bullets[itBullets - 1] = temp;
+                        itBullets--;
+                        Bullets.resize(itBullets);
+
+                        enemies[l].heath--;
+
+ /*                       if (enemies[l].heath == 0)  
+                        {
+                            Enemy temp = enemies[l];
+                            enemies[l] = enemies[enemies.size() - 1];        This is error
+                            enemies[enemies.size() - 1] = temp;              This is error
+                            enemies.resize(enemies.size() - 1);
+
+                            Enemy::count--;
+                        } */
+                    }
+
+                    if (itBullets == k) //Чтобы не случилось выхода за пределы вектора
+                    {
+                        break;
+                    }
+                }
+            }
+
+            for (int k = 0; k < itBullets; ++k) //Ïðîâåðêà ïóëè íà ïîïàäåíèå â ïðåïÿòñòâèå
+            {
+                for (size_t i = 0; i < objects.obj.size(); ++i)
+                {
+                    float t_x = abs((Bullets[k].r0 + Bullets[k].size * Bullets[k].direction + Bullets[k].it * Bullets[k].velocity * Bullets[k].direction).x - objects.obj[i].getPosition().x);
+                    float t_y = abs((Bullets[k].r0 + Bullets[k].size * Bullets[k].direction + Bullets[k].it * Bullets[k].velocity * Bullets[k].direction).y - objects.obj[i].getPosition().y);
+
+                    if ((t_x < objects.sizeObj / sqrt(2)) && (t_y < objects.sizeObj / sqrt(2)))
                     {
                         Bullet temp;
                         temp = Bullets[k];
@@ -176,99 +210,43 @@ struct Scene: public sf::RenderWindow{
                         itBullets--;
                         Bullets.resize(itBullets);
                     }
-                }
 
-                for (int k = 0; k < itBullets; ++k) //Ïðîâåðêà ïóëè íà ïîïàäåíèå âî âðàãà
-                {
-                    for (int l = 0; l < Enemy::count; ++l)
+                    if (itBullets == k) //Чтобы не случилось выхода за пределы вектора
                     {
-                        float t_x = abs((Bullets[k].r0 + Bullets[k].size * Bullets[k].direction + Bullets[k].it * Bullets[k].velocity * Bullets[k].direction).x - enemies[l].getPosition().x);
-                        float t_y = abs((Bullets[k].r0 + Bullets[k].size * Bullets[k].direction + Bullets[k].it * Bullets[k].velocity * Bullets[k].direction).y - enemies[l].getPosition().y);
-
-                        if ((t_x < enemies[l].radius) && (t_y < enemies[l].radius))
-                        {
-                            Bullet temp;
-                            temp = Bullets[k];
-                            Bullets[k] = Bullets[itBullets - 1];
-                            Bullets[itBullets - 1] = temp;
-                            itBullets--;
-                            Bullets.resize(itBullets);
-                        }
-
-                        if (itBullets == k) //×òîáû íå ïðîèçîøëî âûõîäà çà ãðàíèöû âåêòîðà
-                        {
-                            break;
-                        }
-                    }
-                 }
-
-                for (int k = 0; k < itBullets; ++k) //Ïðîâåðêà ïóëè íà ïîïàäåíèå â ïðåïÿòñòâèå
-                {
-                    for (size_t i = 0; i < objects.obj.size(); ++i)
-                    {
-                        float t_x = abs((Bullets[k].r0 + Bullets[k].size * Bullets[k].direction + Bullets[k].it * Bullets[k].velocity * Bullets[k].direction).x - objects.obj[i].getPosition().x);
-                        float t_y = abs((Bullets[k].r0 + Bullets[k].size * Bullets[k].direction + Bullets[k].it * Bullets[k].velocity * Bullets[k].direction).y - objects.obj[i].getPosition().y);
-
-                        if ((t_x < objects.sizeObj / sqrt(2)) && (t_y < objects.sizeObj / sqrt(2)))
-                        {
-                            Bullet temp;
-                            temp = Bullets[k];
-                            Bullets[k] = Bullets[itBullets - 1];
-                            Bullets[itBullets - 1] = temp;
-                            itBullets--;
-                            Bullets.resize(itBullets);
-                        }
-
-                        if (itBullets == k) //×òîáû íå ïðîèçîøëî âûõîäà ïðå ãðàíèöû âåêòîîðà
-                        {
-                            break;
-                        }
+                        break;
                     }
                 }
+            }
 
+            for (int k = 0; k < itBullets; ++k) //Проверка на попадение пули в героя
+            {
+                float t_x = abs((Bullets[k].r0 + Bullets[k].size * Bullets[k].direction + Bullets[k].it * Bullets[k].velocity * Bullets[k].direction).x - hero.getPosition().x);
+                float t_y = abs((Bullets[k].r0 + Bullets[k].size * Bullets[k].direction + Bullets[k].it * Bullets[k].velocity * Bullets[k].direction).y - hero.getPosition().y);
+
+                if ((t_x < hero.radius) && (t_y < hero.radius))
+                {
+                    Bullet temp;
+                    temp = Bullets[k];
+                    Bullets[k] = Bullets[itBullets - 1];
+                    Bullets[itBullets - 1] = temp;
+                    itBullets--;
+                    Bullets.resize(itBullets);
+
+                    hero.heath--; 
+                    cout << hero.heath << endl;
+                }
+
+                if (itBullets == k) //Чтобы не случилось выхода за пределы вектора
+                {
+                    break;
+                }
             }
         }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    }
 
-//    void handleBullets(vector<Bullet> &Bullets, float &prevTimeShoott, int &itBullets){
-//        time = clock.getElapsedTime();
-//        curTime1 = time.asMilliseconds();
-//        if(curTime1 - prevTimeShoott > renderTimeShoot){
-//            prevTimeShoott = curTime1;
-//                for(int k = 0; k < itBullets; ++k) {
-//                    Bullets[k].it += 5;
-//                    if(Bullets[k].it  > width){
-//                        Bullet temp;
-//                        temp = Bullets[k];
-//                        Bullets[k] = Bullets[itBullets - 1];
-//                        Bullets[itBullets - 1] = temp;
-//                        itBullets--;
-//                        Bullets.resize(itBullets);
-//                    }
-//                 }
-//        }
-//    }
-
-
-    sf::VertexArray counterWayBullet(Bullet Bullet){
-        sf::VertexArray b(sf::Lines,2);
-        b[0] = Bullet.r0 +  Bullet.it * Bullet.velocity * Bullet.direction;
+    sf::VertexArray counterWayBullet(Bullet Bullet) {
+        sf::VertexArray b(sf::Lines, 2);
+        b[0] = Bullet.r0 + Bullet.it * Bullet.velocity * Bullet.direction;
         b[1] = Bullet.r0 + Bullet.size * Bullet.direction + Bullet.it * Bullet.velocity * Bullet.direction;
         b[0].color = sf::Color::White;
         b[1].color = sf::Color::White;
@@ -276,6 +254,3 @@ struct Scene: public sf::RenderWindow{
     }
 
 };
-
-
-
